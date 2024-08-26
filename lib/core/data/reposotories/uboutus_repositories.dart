81dd98dd55d9e0103b0f3dfa033bb487fe69abc7
homam ramadan <84981821+homamram@users.noreply.github.com>
2024-08-26 22:38:34 +0300

    Add files via upload
    add lib file

---
+++ lib/core/data/reposotories/uboutus_repositories.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_templete/core/data/models/apis/aboutus_model.dart';
+import 'package:flutter_templete/core/data/models/common_response.dart';
+import 'package:flutter_templete/core/data/network/endpoints/profile_endpoints.dart';
+
+import '../../enums/request_type.dart';
+import '../../utils/network_utils.dart';
+import '../network/network_config.dart';
+
+class AboutUsRepositories {
+  Future<Either<String,AboutUsModel>> aboutUs() async {
+    try {
+      return NetworkUtil.sendRequest(
+        type: RequestType.GET,
+        url: ProfileEndpoints.aboutUs,
+        headers: NetworkConfig.getHeaders(
+            needAuth: true, requestType: RequestType.GET),
+      ).then((response) {
+        CommonResponseModel<dynamic> commonResponse =
+        CommonResponseModel.fromJson(response);
+        if (commonResponse.getStatus &&
+            response['response']['status'] == true) {
+          return Right(AboutUsModel.fromJson(commonResponse.data ?? {}));
+        } else {
+          return Left(commonResponse.message ?? '');
+        }
+      });
+    } catch (e) {
+      return Left(e.toString());
+    }
+  }
+
+}
